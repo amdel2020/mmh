2e50cdb16e8cfd60384f4f1ee7111ecfcff6f327
Amit Sharma <amdel2020@gmail.com>
2020-03-09 00:20:23 +0530

    Refactored full time class

--- lib/app/fullTime.dart
+++ lib/app/fullTime.dart
@@ -30,73 +30,97 @@ class _FullTimeServiceState extends State<FullTimeService> {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: dummyData.maids.map<Widget>((md) {
-            return Card(
-              elevation: 5.0,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 4,
-                    child: Container(
-                      height: 150,
-                      child: Icon(
-                        Icons.account_circle,
-                        size: 120.0,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Container(
-                      height: 180,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              md.name,
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w700
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              'INR. ${md.charges}',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              'Avg. Rating. ${md.rating}',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              md.availability ? 'Available' : 'Not Available',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: md.availability ? Colors.green : Colors.red
-                              ),
-                            ),
-                          ],
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                        ),
-                      ),
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+          children: <Widget>[
+            Text(
+              'Full time maids',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w600,
               ),
-              color: Color.fromRGBO(0, 172, 233, 1),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
+            ),
+            SizedBox(height: 20.0),
+            Column(
+              children: dummyData.maids.map<Widget>((md) {
+                return _item(md);
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _item(Maid md) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 5.0,
+        child: Row(
+          children: <Widget>[
+            _itemImage(),
+            _itemDetails(md),
+          ],
+        ),
+        color: Color.fromRGBO(0, 172, 233, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+    );
+  }
+
+  Expanded _itemDetails(Maid md) {
+    return Expanded(
+      flex: 5,
+      child: Container(
+        height: 180,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                md.name,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
               ),
-            );
-          }).toList(),
+              SizedBox(height: 10.0),
+              Text(
+                'INR. ${md.charges}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Avg. Rating. ${md.rating}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                md.availability ? 'Available' : 'Not Available',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: md.availability ? Colors.green : Colors.red),
+              ),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        ),
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Expanded _itemImage() {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        height: 150,
+        child: Icon(
+          Icons.account_circle,
+          size: 120.0,
         ),
       ),
     );
